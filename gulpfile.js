0dbc62365bb96a90482d2a192e00251bb7334868
Igor Vieira <igor_vieira_matos@hotmail.com>
2025-06-02 22:30:57 -0400

    criando o hero e importando a fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
 function styles () {
-    return gulp.src ('src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('dist/styles'));
+    return gulp.src('./src/styles/main.scss') // apenas este
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('src/styles/*.scss', gulp.parallel(styles))
+function images () {
+    return gulp.src ('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: './' // pasta onde está seu index.html
+        }
+    });
+
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+exports.default = gulp.parallel(styles, images);
+exports.watch = serve;
