55423f1d7ac2b2d812e677368445ac5ce82c4f3b
Igor Vieira <igor_vieira_matos@hotmail.com>
2025-07-26 10:06:40 -0400

    configurano o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify');
+
+function scripts () {
+    return gulp.src('./src/scripts/*.js') 
+    .pipe (uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles () {
     return gulp.src('./src/styles/main.scss') // apenas este
@@ -24,7 +31,8 @@ function serve() {
 
     gulp.watch('./src/styles/*.scss', styles);
     gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./src/styles/*.js', gulp.parallel(scripts)); // fazer minificaçao do arquivo
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = serve;
